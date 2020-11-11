54be2c3d29fa0fc43946b174ea5c54ad3ed4ffba
Quesnel maxime <maxime.quesnel.contact@gmail.com>
2020-11-11 15:51:49 +0100

    Change background color

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,34 +1,41 @@
-import React from 'react'
-import axios from "axios"
-import "./Landing.scss"
+import React from 'react';
+import axios from 'axios';
+import './Landing.scss';
 
 class Landing extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            series: null
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      series: null
+    };
+  }
 
-    componentDidMount() {
-        console.log(process.env)
-        axios.get(`https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.REACT_APP_API_TOKEN}&language=en-FR`).then((res) => this.setState({ series: res.data.results.splice(0,5) }))
-    }
+  componentDidMount() {
+    console.log(process.env);
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.REACT_APP_API_TOKEN}&language=en-FR`
+      )
+      .then(res => this.setState({ series: res.data.results.splice(0, 5) }));
+  }
 
-    render() {
-        return (
-            <div onClick={()=>{console.log(this.state)}}>
-                {this.state.series &&
-                    <React.Fragment>
-                        {this.state.series.map((serie)=>(
-                            <div>{serie.original_name}</div>
-                        ))}
-                    </React.Fragment>
-                }
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        onClick={() => {
+          console.log(this.state);
+        }}
+      >
+        {this.state.series && (
+          <React.Fragment>
+            {this.state.series.map(serie => (
+              <div>{serie.original_name}</div>
+            ))}
+          </React.Fragment>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Landing 
+export default Landing;
