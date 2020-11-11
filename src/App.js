6cf1b80ef76f28b10dc79629e64b3aebba9571a9
yohannmartinez <yohannmartinez34@gmail.com>
2020-11-11 14:27:44 +0100

    create landing, router and install node sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//components
+import Landing from './Components/Landing/Landing'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Landing.scss"
+
+class Landing extends React.Component {
+    render() {
+        return (
+            <div>Landing</div>
+        )
+    }
+}
+
+export default Landing 
